9ac4e57df69f752d2aac000cb9936c17252a6784
mccrackenn <mccrackenn@gmail.com>
2021-10-02 11:31:37 -0700

    adding node-express project

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+
+const app = express();
+const port = 4000;
+
+app.use(express.static("public"));
+app.use(expressLayouts);
+app.set("layout", "./layouts/full-width");
+app.set("view engine", "ejs");
+
+app.get("", (req, res) => {
+  res.render("index", { title: "Home Page" });
+});
+
+//Team Member Routes
+app.get("/Neil", (req, res) => {
+  res.render("neil", { title: "Neil Page" });
+});
+//Team Members Add Routes
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
